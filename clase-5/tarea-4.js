4551cf6ebe9cd3daad319e1dc0a720ebb59c0adf
amShuri <106229595+amShuri@users.noreply.github.com>
2023-06-02 21:05:56 -0300

    Completo tarea y subo la solución

---
+++ clase-5/tarea-4.js
@@ -0,0 +1,123 @@
+// CONSIGNA:
+/* Crear una lista de <ol> y <li> que contengan sólo números.
+ * Convertir esos números a un array y:
+ * 1. Calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+ * 2. Obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+ * 3. Obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+ * 4. Obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+ */
+
+function obtenerNumerosAleatorios() {
+  return Math.floor(Math.random() * 100);
+}
+
+function asignarNumerosAleatorios() {
+  const $listaItems = document.querySelectorAll('li');
+
+  for (let i = 0; i < $listaItems.length; i += 1) {
+    $listaItems[i].textContent = obtenerNumerosAleatorios();
+  }
+}
+
+function obtenerListaNumeros() {
+  const $listaItems = document.querySelectorAll('li');
+  const listaNumeros = [];
+
+  for (let i = 0; i < $listaItems.length; i += 1) {
+    const numerosTexto = Number($listaItems[i].textContent);
+    listaNumeros.push(numerosTexto);
+  }
+  return listaNumeros;
+}
+
+function obtenerPromedio() {
+  const listaNumeros = obtenerListaNumeros();
+  let promedio = 0;
+
+  for (let i = 0; i < listaNumeros.length; i += 1) {
+    promedio += listaNumeros[i] / listaNumeros.length;
+  }
+  return Math.floor(promedio);
+}
+
+function obtenerNumeroMasChico() {
+  const listaNumeros = obtenerListaNumeros();
+  let numeroMasChico = listaNumeros[0];
+
+  for (let i = 0; i < listaNumeros.length; i += 1) {
+    if (numeroMasChico > listaNumeros[i]) {
+      numeroMasChico = listaNumeros[i];
+    }
+  }
+  return numeroMasChico;
+}
+
+function obtenerNumeroMasGrande() {
+  const listaNumeros = obtenerListaNumeros();
+  let numeroMasGrande = listaNumeros[0];
+
+  for (let i = 0; i < listaNumeros.length; i += 1) {
+    if (numeroMasGrande < listaNumeros[i]) {
+      numeroMasGrande = listaNumeros[i];
+    }
+  }
+  return numeroMasGrande;
+}
+
+function obtenerNumeroMasFrecuente() {
+  const listaNumeros = obtenerListaNumeros();
+  let contadorMaximo = 0;
+  let numeroMasFrecuente = 0;
+
+  for (let i = 0; i < listaNumeros.length; i += 1) {
+    let contadorActual = 0;
+    for (let j = 0; j < listaNumeros.length; j += 1) {
+      if (listaNumeros[i] === listaNumeros[j]) {
+        contadorActual += 1;
+      }
+
+      if (contadorActual > contadorMaximo) {
+        contadorMaximo = contadorActual;
+        numeroMasFrecuente = listaNumeros[i];
+      }
+    }
+  }
+  return contadorMaximo > 1 ? numeroMasFrecuente : 'No hay valores repetidos';
+}
+
+function mostrarResultados() {
+  const $promedio = document.querySelector('#promedio');
+  const $numeroMasChico = document.querySelector('#numero-mas-chico');
+  const $numeroMasGrande = document.querySelector('#numero-mas-grande');
+  const $numeroMasFrecuente = document.querySelector('#numero-mas-frecuente');
+
+  $promedio.textContent += obtenerPromedio();
+  $numeroMasChico.textContent += obtenerNumeroMasChico();
+  $numeroMasGrande.textContent += obtenerNumeroMasGrande();
+  $numeroMasFrecuente.textContent += obtenerNumeroMasFrecuente();
+}
+
+function resaltarResultados() {
+  const li = document.querySelectorAll('li');
+  const numeroMasChico = obtenerNumeroMasChico();
+  const numeroMasGrande = obtenerNumeroMasGrande();
+  const numeroMasFrecuente = obtenerNumeroMasFrecuente();
+
+  for (let i = 0; i < li.length; i += 1) {
+    if (li[i].textContent == numeroMasChico) {
+      li[i].style.color = '#20b2aa';
+    }
+
+    if (li[i].textContent == numeroMasGrande) {
+      li[i].style.color = '#f08080';
+    }
+
+    if (li[i].textContent == numeroMasFrecuente) {
+      li[i].style.color = '#ec5fb6';
+    }
+  }
+}
+
+asignarNumerosAleatorios();
+mostrarResultados();
+resaltarResultados();
